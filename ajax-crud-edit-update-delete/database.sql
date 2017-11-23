713eb742c18c80e5b2a311ab7668f5969a878abe
vasimwalikar <vasim143s@gmail.com>
2017-11-23 17:12:50 +0530

    updated on 23-11-17

---
+++ ajax-crud-edit-update-delete/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=63 ;
